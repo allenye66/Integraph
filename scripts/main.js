a5164d1ac045d3189578b402e9118bc3e3531be3
Allen <allenye66@gmail.com>
2021-09-30 17:25:58 +0100

    draws the background of the graph

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,109 @@
 var canvas = document.getElementById("myCanvas"), c = canvas.getContext('2d');
 
 
+
+//the initial website starts with a graph of x^2
 math = mathjs(),
-expr = 'sin(x)',
+expr = 'x^2',
 scope  = {x: 0}, 
 tree = math.parse(expr, scope);
+
+draw_grid();
+draw_bold();
 drawCurve();
+
 get_input();
+
+
+function draw_grid(){
+
+    //vertical lines
+    for(var i = 0; i < canvas.width; i += canvas.width/50){
+        c.moveTo(i, 0);
+        c.lineTo(i, canvas.height);
+        c.strokeStyle = "#f0f0f0"
+        c.stroke();   
+        c.lineWidth = 1;
+    }
+    //horizontal lines
+    for(var j = 0; j < canvas.height; j += canvas.height/50){
+        c.moveTo(0, j)
+        c.lineTo(canvas.width, j);
+        c.strokeStyle = "#f0f0f0"
+        c.stroke();   
+        c.lineWidth = 1;
+    }
+    c.beginPath();
+}
+
+function draw_bold(){
+ //vertical bold lines
+    for(var k = 0; k < canvas.width; k += canvas.width/10){
+        c.moveTo(k, 0);
+        c.lineTo(k, canvas.height);
+        c.lineWidth = 1;
+        c.strokeStyle = "#c0c0c0"
+        c.stroke(); 
+    }
+    
+    //horizontal bold lines
+    for(var l = 0; l < canvas.height; l += canvas.height/10 ){
+        c.moveTo(0, l)
+        c.lineTo(canvas.width, l);
+        c.strokeStyle = "#c0c0c0"
+        c.lineWidth = 1;
+        c.stroke();
+ 
+        
+    }
+    c.beginPath();
+}
+
+
+function draw_x_axis(){
+    
+    c.moveTo(0, canvas.height/2)
+    c.lineTo(canvas.width, canvas.height/2);
+    c.strokeStyle = "black"
+    c.lineWidth = 3; 
+    c.stroke();
+    c.beginPath();
+    
+    
+    //add numbers
+}
+
+function draw_y_axis(){
+    
+    c.moveTo(canvas.width/2, 0);
+    c.lineTo(canvas.width/2, canvas.height);
+    c.strokeStyle = "black"
+    c.lineWidth = 3; 
+    c.stroke();
+    
+    
+    c.font = "12px Arial";
+    c.fillText(1, canvas.width/2 + 18, canvas.height/2 + 15);
+    
+    c.beginPath();
+    
+    
+    //add numbers
+    //for(var i = 0; i <= canvas.width; i += canvas.height/10){
+        
+        
+    //}
+    
+    
+}
+
+
+function draw_background(){
+    draw_grid();
+    draw_bold();
+    draw_x_axis();
+    draw_y_axis();
+}
 
 function drawCurve() {
     
@@ -15,6 +112,12 @@ function drawCurve() {
     
     
     c.clearRect(0, 0, canvas.width, canvas.height);
+    
+    
+    
+    draw_background();
+    
+    
     //number of points
     n = 1000
     
@@ -24,12 +127,9 @@ function drawCurve() {
     yMin = -10;
 
 
-    
     c.beginPath();
     
     for(var i = 0; i < n; i ++){
-    
-        
         
         percentX = i /(n-1);
         //generates a percentages of the interval n
@@ -62,6 +162,8 @@ function drawCurve() {
         c.lineTo(xPixel , yPixel);
 
     }
+    c.strokeStyle = "red"
+    c.lineWidth = 3;
     c.stroke();
 
     
@@ -81,7 +183,6 @@ function get_input(){
         tree = math.parse(expr, scope)
         drawCurve();
     });
-    
 }
     
 
